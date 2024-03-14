74100aa07ce835cc2aa0f96b1ffc95f44a769cd7
AngeliqueMarachev <angeliquealive@gmail.com>
2024-03-14 14:18:24 +0200

    Add user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @desc Register a user
 // @access Public
 
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Valid email is required").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // See if user exists
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
