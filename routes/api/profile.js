4dac99befc780a3bb002c3d40eca8df4f0499c5a
AngeliqueMarachev <angeliquealive@gmail.com>
2024-03-31 14:48:12 +0300

    Add route for delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,24 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // TODO: remove users posts
+
+    // Delete profile
+    await Profile.findOneAndDelete({ user: req.user.id });
+
+    // Delete user
+    await User.findOneAndDelete({ _id: req.user.id });
+
+    res.json({ msg: "User deleted successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
