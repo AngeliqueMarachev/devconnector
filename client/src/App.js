48224d057ab7e853b6a79edfd9c278070925fbf7
AngeliqueMarachev <angeliquealive@gmail.com>
2024-04-03 11:52:18 +0300

    Add Register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 const App = () => (
   <Fragment>
     <Navbar />
     <Landing />
+    <Register />
   </Fragment>
 );
 
